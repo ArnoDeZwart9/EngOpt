a8391221d1c1d1cabed7d7f709359f07f4b1b5d0
Arno de Zwart <apdezwart@hotmail.nl>
2025-04-07 12:06:34 +0200

    All four algorithms implemented

--- Project/optim_main.m
+++ Project/optim_main.m
@@ -11,27 +11,39 @@ usage = transpose(readmatrix("data\usage.csv"));
 
 %% run objective 
 format('long');
+x0 = [90 30 30];
+lb = [0 0 0];
+ub = [360 90 100];
 
+%% Nelder-Mead simplex 
 Nelder_options = optimset(...
     'Display','iter', ...
     'MaxFunEvals', 2e4);
 
-x0 = [90 30 30];
-x_Nelder = fminsearch(@(x) objfun_disc(x,azimuth, elevation, irradiation, usage), x0, Nelder_options);
-% x_Anneal = simulannealbnd(@(x) objfun_disc(x,azimuth, elevation, irradiation, usage), x0, Anneal_options)
+[x_Nelder, f_Nelder] = fminsearch(@(x) objfun_disc(x,azimuth, elevation, irradiation, usage), x0, Nelder_options);
 
+%% Simulated Annealing algorithm.
+Anneal_options = optimoptions('simulannealbnd', ...
+    'Display','iter', ...
+    'MaxFunctionEvaluations', 1e3);
 
-disp(x_Nelder)
+[x_Anneal, f_Anneal] = simulannealbnd(@(x) objfun_disc(x,azimuth, elevation, irradiation, usage), x0, lb, ub, Anneal_options);
+
+%% Genetic algorithm
+GA_options = optimoptions("ga", ...
+    Display="iter");
+
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+
+[x_GA, f_GA] = ga(@(x) objfun_disc(x,azimuth, elevation, irradiation, usage), 3, A, b, Aeq, beq, lb, ub);
+
 %% laatste keer
+Swarm_options = optimoptions("particleswarm", ...
+    Display="iter");
 
+[x_Swarm, f_Swarm] = particleswarm(@(x) objfun_disc(x,azimuth, elevation, irradiation, usage), 3, lb, ub, Swarm_options);
 
 %% plotting
-% 
-% time = 1:24;
-% 
-% figure(1); clf(1);
-% hold on
-% for i = 1:12
-%     plot(time, f(time, i))
-% end
-% legend({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'}, Location="southwest")
